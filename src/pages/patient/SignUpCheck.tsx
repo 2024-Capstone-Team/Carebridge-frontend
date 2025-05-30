3ccac32775372b15dd5da4ca760fec6cbe8c715b
alicek0 <alicekangisy@gmail.com>
2025-05-30 23:17:52 +0900

    fix: add 9:00 to hospitalizationDate datetimeFormat

--- src/pages/patient/SignUpCheck.tsx
+++ src/pages/patient/SignUpCheck.tsx
@@ -46,7 +46,7 @@ function SignUpCheck() {
 
     // 먼저 환자 정보 등록 시도
     try {
-      const today = new Date().toISOString().split(".")[0];
+      const today = new Date().toISOString().split(".")[0]+"9:00";
       const patientData = {
         name,
         phoneNumber,
